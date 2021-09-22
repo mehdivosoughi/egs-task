2b09026915d3633385bd1d04aff2227c52802743
Mehdi Vosoughi <mehdi.vosoughi84@gmail.com>
2021-09-22 23:00:06 +0330

    initialize project

---
+++ src/main/java/com/example/egstask/EgsApplication.java
@@ -0,0 +1,13 @@
+package com.example.egstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgsApplication.class, args);
+	}
+
+}
